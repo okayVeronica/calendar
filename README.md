cfccf1491e6ca804d629a9abb8b5dc2e2bb8df8a
veronica borges <veronica.bores@alunoescolassociais.org.br>
2023-09-19 11:31:37 -0300

    Edição do README

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # calendar
+
+[Calendario](#calendar)
+[Descrição] 
+[Introdução]
+[Funcionalidade]
+[Tecnologias.ultilizadas]
+[Fontes.consultadas]
+[Autores]
+
+# Calendar📆
+
+![Capa do projeto]
+()
+
+
+## Descrição 
+
+## Introdução
+
+
+## Tecnologias ultilizadas 
+
+ * VS Code(Visual Code)
+
+ * Github
+
+ * HTML
+
+ * CSS
+ 
+ * JSS
+
+## Fontes consultadas 
+Leonardo Rocha
+
+## Autores 
+* Veronica Borges
--- table.js
+++ table.js
@@ -1,10 +1,8 @@
-function colorirData(){
-    let day         = document.getElementById('day').value;
-    let color       = document.getElementById('color').value;
-    let calendar    = document.getElementById('calendar');
-    //Forma 1 - solução 
-    let putColor    = calendar.getElementsByTagName('td')[parseInt(day)+5];
-     putColor.style.backgroundColor = color; 
-    //Forma 2 - solução
-     // calendar.getElementsByTagName('td')[parseInt(day)+5].style.backgroundColor = color;
+function colorirData() {
+    let day = document.getElementById('day').value;
+    let color = document.getElementById('color').value;
+    let calendar = document.getElementById('calendar');
+    let putColor = calendar.getElementsByTagName('td')[parseInt(day)+5];
+    putColor.style.backgroundColor = color;
+    
 }
